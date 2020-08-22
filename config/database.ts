fc35261e07db1d92426da28b68ca70813ec9e9e3
Caio Fernando <caiofsr13@gmail.com>
2020-08-22 15:36:33 -0300

    fix: :arrow_up: fixing url database to heroku

--- config/database.ts
+++ config/database.ts
@@ -80,13 +80,14 @@ const databaseConfig: DatabaseConfig & { orm: Partial<OrmConfig> } = {
     */
     pg: {
       client: 'pg',
-      connection: {
-        host: Env.get('DB_HOST', '127.0.0.1') as string,
-        port: Number(Env.get('DB_PORT', 5432)),
-        user: Env.get('DB_USER', 'lucid') as string,
-        password: Env.get('DB_PASSWORD', 'lucid') as string,
-        database: Env.get('DB_NAME', 'lucid') as string,
-      },
+      connection: Env.get('DATABASE_URL') as string,
+      // {
+      //   host: Env.get('DB_HOST', '127.0.0.1') as string,
+      //   port: Number(Env.get('DB_PORT', 5432)),
+      //   user: Env.get('DB_USER', 'lucid') as string,
+      //   password: Env.get('DB_PASSWORD', 'lucid') as string,
+      //   database: Env.get('DB_NAME', 'lucid') as string,
+      // },
       healthCheck: false,
     },
   },
